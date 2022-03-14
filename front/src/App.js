1ccaf922d4f1a2cd2428f7ac58f21a1769057f95
Weiling <bwlw3127@gmail.com>
2022-03-14 21:34:40 +0800

    Create App.js

---
+++ front/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import HomePage from './cpn/HomePage';
+import Article from './cpn/Article';
+import {
+  BrowserRouter as Router, 
+  Route,
+  Routes
+} from 'react-router-dom'
+
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage/>}></Route>
+          <Route path="/article/:Id" element={<Article/>}></Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
